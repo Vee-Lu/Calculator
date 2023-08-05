e3810d76daf3be1e3eb085b8d65d524190c1d4bb
Vincent Lu <luviny2022@gmail.com>
2023-08-05 14:05:15 -0500

    Finished project

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # Calculator
 A calculator application built through the use of HTML, CSS, and JS
+
+Looking back on this project, the hardest obstacle I had to overcome when trying to complete it was actually starting it.
+
+When I was trying to create the actual layout of the calculator inside HTML and CSS, I had a very hard time trying to actually create the image of 
+a calculator. I think the main reason why I was so worked up on the image part of the calculator and not the function of it was because I wanted
+everything to be perfect on the get-go.
+
+This is actually a very big problem for me. Whenever I start a project, I wanted everything to be perfect along the way, with no setback whatsover.
+However, trying to make it perfect initally just made me overthink what I was doing, and not ACTUALLY coding the calculator.
+My strive for perfection resulted in me procrastinating, which is why it took longer than it should have.
+
+My thoughts for perfection got worse once I looked at other people's calculator (big mistake right there) and I just felt so tiny and not qualified
+for this whatsover.
+
+However, I needed to get over my thoughts of perfection. If I just kept thinking what it looked like, I will never get anything done.
+So I just dived into making the calculator, not actually caring how it looked like. I needed to actually DO it, and then I can worry about my imperfections later.
+
+I finished the calculator now, and I am proud to say that I am extremely proud of the result. It might not be as fancy as the others, but it 
+WORKS, and that's the most important part. I even managed to do the extra credit they asked like having a decimal and backspace button. I even
+went a little extra, and added a sign button, and transiton effect.
+
+So my advice to others? When caught in the web of perfection, just dive into it. You'll reach for perfection that way instead of thinking
+of it.
---
+++ calculator.js
@@ -0,0 +1,203 @@
+//All the basic mathematical operations of a calculator
+function add(a,b) { return a+b};
+function subtract(a,b) { return a-b};
+function multiply(a,b) { return a*b};
+function divide(a,b) { return a/b};
+
+//Set the a,b, and the operators to initally blank
+var a = b = op =  "";
+
+//Grab the numbers and operators element
+var numbers = document.querySelectorAll(".num");
+var operators = document.querySelectorAll(".operator");
+
+//Get the display element and set the display text to be blank
+var displayText = "";
+var display = document.querySelector("#display");
+
+/**
+ * Function that calls one of the calculators operators depending on the operator symbol
+ * @param {*} a,one of the values to be used in an operation
+ * @param {*} b, one of the other values to be used in an operation
+ * @param {*} op, the sign indicating which operator to use
+ * @returns 
+ */
+function operate(a,b,op){
+    switch(op){
+        case "+":
+            console.log("+");
+            return add(a,b);
+        case "-":
+            console.log("-");
+             return subtract(a,b);
+        case "*":
+            console.log("*");
+            return multiply(a,b);
+        default:
+            console.log("/");
+            return divide(a,b);
+    }
+}
+
+
+//Event listener that displays the number clicked on the calculator
+numbers.forEach(number => number.addEventListener("click", function() {
+    if(op == "")
+    {
+        a += number.textContent;
+        console.log("A: " + a);
+        displayText += number.textContent;   
+    }
+
+    else 
+    {
+        b += number.textContent;
+        console.log("B: " + b);
+        displayText += number.textContent;   
+    }
+
+    display.innerText = displayText;   
+}));
+
+//Grab the clear element and when clicked, clears the A,B, and Operator elements
+var clear = document.querySelector("#clear");
+clear.addEventListener("click", function() { 
+    displayText = ""; 
+    a = b = operator = displayText; 
+    display.innerText = displayText;
+});
+
+//Grab the delete element
+var deleteButton = document.querySelector("#delete");
+//When we press delete, it will backspace a character 
+deleteButton.addEventListener("click", function(){
+    //If display text is not empty, we remove the last character
+    if(displayText != "")
+    {
+        displayText = displayText.slice(0,displayText.length-1);
+        //If A, Operator and B are occupied, remove last character from B
+        if (a != "" && op != "" && b!= "")
+        {
+            b = b.slice(0,b.length-1);
+        }
+        //If A and Operator are occupied, remove character from Op
+        else if (a != "" && op != "")
+        {
+            op = "";
+        }
+        //If A is occupied, remove last character from A
+        else if (a != "")
+        {
+            a = a.slice(0,a.length-1);
+        }
+
+        //Display the result to the screen
+        display.innerText = displayText;
+    } 
+   
+});
+
+//Grab the sign element
+var sign = document.querySelector("#sign");
+//If the sign element is clicked, change A to positive or negative
+sign.addEventListener("click", function(){
+    if(a == "" || a != "" && b == "")
+    {
+        //If A already has a negative sign, remove it by using the slice method to get the entirety of A except for character 0, which is "-"
+        if(a.includes("-"))
+        {
+            //Do the same thing for the displayText and show it to the screen
+            a = a.slice(1,a.length);
+            displayText = displayText.slice(1,displayText.length);
+            display.innerText = displayText;
+        }
+        else
+        //If A is positive, we add a negative sing to A through the use of concatenation
+        {
+            //Do the same thing for the displayText and show it to the screen
+            a = "-" + a;
+            displayText = "-" + displayText;
+            display.innerText = displayText;
+        }
+    }
+});
+
+
+//Grab the decimal element
+var decimal = document.querySelector("#decimal");
+decimal.addEventListener("click", function(){
+    //If A is not blank and the Operator is blank (add decimal after number) OR A is blank (add decimal before number)
+    //Then add a decimal to A
+    if(a != "" && op == "" || a == "")
+    {
+        if(a.includes(".")) return;
+
+        a += decimal.innerText;
+        displayText += decimal.innerText;
+
+    }
+
+    //If A has a number and there is an operator, we can add a decimal to B
+    else if(a != "" && op != "")
+    {
+        if(b.includes(".")) return;
+        b += decimal.innerText;
+        displayText += decimal.innerText;
+    }
+
+    display.innerText = displayText;   
+});
+
+//When an operator button has been pressed, store it into the display text and have the operator variable store it
+operators.forEach(operator => operator.addEventListener("click", function() {  
+   
+    //If a is not blank, and the operator is blank, we can add an operator
+    if (a != "" && op == "")
+    {
+        op = operator.textContent;
+
+        //Add the operator symbol to the display text and display it to the screen
+        displayText += op;
+        console.log("Op: " + op);
+        display.innerText = displayText;
+    } 
+
+    //If A, B and the Operator are not blank, and we attempt to add another operator, perform an operation and store the result into A
+    //before adding another operator
+    if (a!= "" && b!= "" && op != "")
+    {
+        //Call the operation method
+        operation();
+        op = operator.textContent;
+
+        //Add the operator symbol to the display text and display it to the screen
+        displayText += op;
+        console.log("Op: " + op);
+        display.innerText = displayText;
+    }
+    
+    
+}))
+
+//Get the equals button
+var equals = document.querySelector("#equals");
+//When the equal button is pressed, call the operation function
+equals.addEventListener("click", operation);
+
+/**
+ * Performs a math operation
+ */
+function operation(){
+    if(a == "" || b == "" || op == "") return;
+
+    a = operate(+a,+b,op);
+    if(!Number.isInteger(a))a = a.toFixed(6);
+    b = op = "";
+    display.innerText =  displayText = a;
+
+    console.log(displayText);
+    console.log("Op A: " + a);
+
+}
+
+  
